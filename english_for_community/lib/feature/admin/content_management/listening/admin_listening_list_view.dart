a403312c982b77363e86537f2821b8210668deed
phanduy2004 <conlocmuahe2004@gmail.com>
2026-05-07 12:52:33 +0700

    .

--- english_for_community/lib/feature/admin/content_management/listening/admin_listening_list_view.dart
+++ english_for_community/lib/feature/admin/content_management/listening/admin_listening_list_view.dart
@@ -52,7 +52,7 @@ class _AdminListeningListBodyState extends State<_AdminListeningListBody> {
     );
 
     if (mounted) {
-      // ✅ Refresh listd TẠI ĐÂY là an toàn nhất
+      // ✅ Refresh listd sTẠI ĐÂY là an toàn nhất
       context.read<AdminListeningBloc>().add(
         const GetAdminListeningListEvent(limit: 9999, page: 1),
       );
